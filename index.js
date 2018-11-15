81a220397b43d089e903b9099a72e5c23be99972
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-15 22:59:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function multiply(n1, n2){
 function divide(n1,n2) {
   return n1 / n2
 }
+
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
+
+function preserve
